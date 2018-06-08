f5b6cfeedd7d9d26a83d4138a560bd968e9aa074
nandpara <nandpara@cisco.com>
2018-06-08 20:17:59 +0530

    Added Logs for Better understanding

--- Collective_Intelligence/Clustering.py
+++ Collective_Intelligence/Clustering.py
@@ -1,9 +1,21 @@
 from Helpers.Logging import *
+import os
+from collectiveintelligence_book_master import clusters
 
+
+blogdata = os.path.dirname(os.path.dirname(__file__)) + '/collectiveintelligence_book_master/blogdata.txt'
+print (blogdata)
 class Clustering :
 
     def __init__(self):
-        Info("This is a Test Log Messgae")
+        blogname, words, data = clusters.readfile(blogdata)
+        Info("{} --> {} --> {}".format(type(blogname), len(blogname), blogname ))
+        Info("{} --> {} --> {}".format(type(words), len(words), words))
+        Info("{} --> {} --> {}".format(type(data), len(data), data))
+
+        clust = clusters.hcluster(data)
+
+
 
 
 if __name__ == '__main__':

0186da29cf47406c33acfe14f3a058464554f892
nandpara <nandpara@cisco.com>
2018-07-11 10:21:03 +0530

    Updated from office july 11

--- Analytics_Vidhya/Find_stock_Price_Volume_difference_from_previous_days.py
+++ Analytics_Vidhya/Find_stock_Price_Volume_difference_from_previous_days.py
@@ -6,9 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 nse = Nse()
 import pickle
-from Helpers import Helpers
+from Helpers.Helpers import Helpers
 from Linear_Regression import Linear_Regression
-Feature_List = ['Volume_percent_change', 'HL_PCT', 'Price_Percent_Change']
+Feature_List = ['Volume_percent_change','Price_Percent_Change', 'HL_PCT']
 Label_List = ['Close']
 class Price_Volume_Difference:
     def __init__(self):
@@ -23,6 +23,7 @@ class Price_Volume_Difference:
         History['HL_PCT'] = (History['High']- History['Low']) / History['Close'] * 100
         # History.['Label'] =
         History = History.dropna()
+        print ("Length of History : ", len(History))
         return History
 
 
@@ -30,12 +31,13 @@ class Price_Volume_Difference:
 if __name__=="__main__":
     obj = Price_Volume_Difference()
     History = obj.Prepare_Stock_History_Dataset(Stock = 'hdfc')
-    print (History.tail()[Feature_List + Label_List])
+    print ( History.tail()[Feature_List])
+    print ( History.tail()[Label_List])
     #Matplot().Line_Plot("Volume_percent_change", History['Volume_percent_change'], "Price_Percent_Change", History['Price_Percent_Change'])
     import numpy as np
 
     Feature = np.array(History[Feature_List])
     Label = np.array(History[Label_List])
-    #Helpers().Print_Type_And_Value(Feature, Label)
+    Helpers().Print_Type_And_Value(Feature, Label)
     Linear_Regression().Predict_With_Linear_Regression(Feature, Label)
 
